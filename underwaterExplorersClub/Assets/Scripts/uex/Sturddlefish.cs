a0647e09e557463b5a00908fb5b23400d1967541
Paul Griffiths <paul@tammydog.com>
2020-11-16 12:00:15 +0000

    many changes re: sturddlefish

---
+++ underwaterExplorersClub/Assets/Scripts/uex/Sturddlefish.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sturddlefish : MonoBehaviour
+{
+
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponentInChildren<Animator>();
+        Debug.Assert(animator != null);
+
+        animator.SetBool("isSwimming", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
